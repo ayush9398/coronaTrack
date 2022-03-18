530f1d72928b3d15d84adf352dc120b385320e0b
Ayush Mittal <ayush.mittal9398@gmail.com>
2022-03-19 00:36:30 +0530

    frontend basic setup

---
+++ frontend/webpack.config.js
@@ -0,0 +1,36 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const path =require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins:[
+        new HTMLWebpackPlugin({
+            template: "./src/index.html"
+        }),
+    ],
+    resolve: {
+        modules: [__dirname, "src", "node_modules"],
+        extensions: ["*", ".js", ".jsx"]
+    },
+    module:{
+        rules:[
+            {
+                test: /\.jsx?$/,
+                exclude: "/node_modules/",
+                loader: require.resolve("babel-loader"),
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+              },
+              {
+                test: /\.png|svg|jpg|gif$/,
+                use: ["file-loader"],
+              }, 
+        ]
+    }
+};
